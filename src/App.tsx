ed438c551e5a92c72e0066a1e99f298d503956c2
Michael Kashapov <michael@truckpartsolutions.com>
2022-12-03 18:53:19 -0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello My new React Template App!
+      </h1>
+    </div>
+  );
+}
